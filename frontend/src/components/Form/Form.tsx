e9e7fffad906b6307c057c64a16fa97abd4f01bc
pablojg9 <pablojuniorgn2@gmail.com>
2021-09-28 17:56:57 -0300

    login completed

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -1,5 +1,9 @@
 import 'assets/css/form.css';
 
+import { BsAward } from 'react-icons/bs';
+import { IoCodeSlashOutline } from 'react-icons/io5';
+import { BsEnvelope } from 'react-icons/bs';
+
 const Form = () => {
    return (
       <>
@@ -7,6 +11,24 @@ const Form = () => {
             <div className="row">
                <div className="col">
                   <h2 className="frase">Find, encontre o profissional ideal para seu projeto</h2>
+
+                  <div className="block-table">
+                     <BsAward size={35} color="1b98e0"/>
+                     <span className="text-dev">Desenvolvedores qualicados</span>
+                  </div>
+
+                  <div className="block-table">
+                     <IoCodeSlashOutline size={35} color="1b98e0"/>
+                     <span className="text-dev">Amplas lista de linguagem</span>
+                  </div>
+
+                  <div className="block-table">
+                     <BsEnvelope size={35} color="1b98e0"/>
+                     <span className="text-dev">Conversa via chat com o profissional</span>
+                     <br />
+                     <button id="btn">Envie seu curriculo</button>
+
+                  </div>
                </div>
                <div className="col">
                   <form>
